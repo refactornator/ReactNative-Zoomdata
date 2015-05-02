3f5e35f17c7feacddad58cec26e6fe20ecd94d68
William Lindner <wlindner522@gmail.com>
2015-05-02 15:53:48 -0700

    Add a logo and password TextInput to the login screen and cleanup the CSS

--- overlays/LoginOverlay.js
+++ overlays/LoginOverlay.js
@@ -6,7 +6,9 @@ var Button = require('react-native-button');
 
 var {
   AppRegistry,
+  LayoutAnimation,
   StyleSheet,
+  Image,
   Text,
   TextInput,
   TouchableHighlight,
@@ -17,17 +19,21 @@ var LoginOverlay = React.createClass({
   getDefaultProps(): StateObject {
     return {
       isVisible: false
-    }
+    };
   },
 
   render() {
     return (
       <Overlay isVisible={this.props.isVisible}>
         <View style={styles.container}>
-          <TextInput style={styles.textInput} />
-          <Button style={styles.button} onPress={this._handlePress}>
-            Login
-          </Button>
+          <Image style={styles.logo} source={require('image!zoomdata')} />
+          <TextInput style={styles.textInput} placeholder="Username" />
+          <TextInput style={styles.textInput} placeholder="Password" password={true} />
+          <TouchableHighlight
+            style={styles.button}
+            onPress={this._handlePress}>
+              <Text style={[styles.button, styles.buttonText]}>LOG IN</Text>
+          </TouchableHighlight>  
         </View>
       </Overlay>
     )
@@ -38,27 +44,52 @@ var LoginOverlay = React.createClass({
   },
 });
 
-
 var styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#575253'
+  },
+  logo: {
+    width: 320,
+    height: 118
   },
   textInput: {
+    paddingLeft: 10,
+    marginTop: 10,
     height: 40,
-    width: 100,
+    width: 320,
     borderColor: 'gray',
     borderWidth: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignSelf: 'center',
   },
   button: {
-    width: 100,
+    marginTop: 10,
+    width: 320,
     height: 50,
-    color: 'orange'
+    backgroundColor: 'black',
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 35,
   }
 });
 
+var animationConfig = {
+  duration: 700,
+  create: {
+    type: LayoutAnimation.Types.spring,
+    springDamping: 0.4,
+    property: LayoutAnimation.Properties.opacity,
+  },
+  update: {
+    type: LayoutAnimation.Types.spring,
+    springDamping: 0.4,
+    property: LayoutAnimation.Properties.opacity,
+  }
+};
 
 module.exports = LoginOverlay;
