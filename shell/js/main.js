47ae54be017e3d3f59ccd3888eadb9ddd85ec9c5
William Lindner <wlindner522@gmail.com>
2015-05-27 15:43:39 -0700

    Show a gauge from the remote server.

--- shell/js/main.js
+++ shell/js/main.js
@@ -1,65 +1,47 @@
 var credentials = {
-  key: '554312fc4568aa27eafbe0ee'
+  key: '556620f4e4b0ecaf733d0f3e'
 };
 
 var application = {
-  secure: false,
-  host: '192.168.42.67', //192.168.42.67
-  port: 8080,
-  path: '/zoomdata'
+    secure: false,
+    host: '52.4.236.245',
+    port: 8080,
+    path: '/zoomdata'
 };
 
 var sourceName = 'Real Time Sales';
 
 var queryConfig = {
-  player: {
-      pauseAfterRead: false,
-      speed: 1,
-      timeWindow: 'ROLLING_HOUR'
-  },
-  time: {
-    timeField: '_ts',
-  },
-  tz: 'UTC',
-  filters: [],
-  groupBy: [{
-    name: 'usercity',
-    limit: 10,
-    sort: {
-      dir: 'desc',
-      name: 'usersentiment'
-    }
-  }],
-  metrics: [{
-    name: 'usersentiment',
-    func: 'avg'
-  }]
+    tz: 'UTC',
+    player: {
+        speed: 1,
+        pauseAfterRead: true
+    },
+    time: {
+        timeField: '_ts'
+    },
+    filters: [],
+    groupBy: [],
+    metrics: [
+        {
+            name: 'price',
+            func: 'avg'
+        }
+    ]
 };
 
-var visualizations = {
-  bubbles: {
-    name: 'Packed Bubbles',
-    variables: {
-      'Bubble Size': 'count',
-      'Bubble Color': 'usersentiment:avg'
+var visualizations = [
+    {
+        name: 'Gauge',
+        variables: {
+            'Metric': 'price:avg'
+        }
     }
-  },
-  donut: {
-    name: 'Donut',
-    variables: {
-      'Size': 'count'
-    }
-  },
-  treeMap: {
-    name: 'Tree Map',
-    variables: {
-      'Size': 'count',
-      'Color': 'count'
-    }
-  }
-};
+];
 
 var swiper = new Swiper('.swiper-container');
+
+debugger;
 
 var client, query;
 ZoomdataSDK.createClient({
@@ -76,25 +58,33 @@ ZoomdataSDK.createClient({
     query = newQuery;
     console.log(query);
 
-    var visualization1 = visualizations['treeMap'];
+    var visualization = visualizations[0];
+      window.client.visualize({
+          element: document.getElementById('visualization'),
+          query: query,
+          visualization: visualization.name,
+          variables: visualization.variables
+      });
 
-    client
-        .visualize({
-            element: document.getElementById('treemap'),
-            query: query,
-            visualization: visualization1.name,
-            variables: visualization1.variables
-        });
+    // var visualization1 = visualizations['treeMap'];
 
-    var visualization2 = visualizations['donut'];
+    // client
+    //     .visualize({
+    //         element: document.getElementById('treemap'),
+    //         query: query,
+    //         visualization: visualization1.name,
+    //         variables: visualization1.variables
+    //     });
 
-    client
-        .visualize({
-            element: document.getElementById('donut'),
-            query: query,
-            visualization: visualization2.name,
-            variables: visualization2.variables
-        });
+    // var visualization2 = visualizations['donut'];
+
+    // client
+    //     .visualize({
+    //         element: document.getElementById('donut'),
+    //         query: query,
+    //         visualization: visualization2.name,
+    //         variables: visualization2.variables
+    //     });
 })
 .catch(onError);
 
